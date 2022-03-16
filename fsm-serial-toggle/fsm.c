74b79b8fc4c4466f9108d8af3c8b8703507e0ab1
waskita <waskita@ee.itb.ac.id>
2022-03-16 21:53:32 +0700

    FSM toggle dengan serial diaktifkan

---
+++ fsm-serial-toggle/fsm.c
@@ -0,0 +1,50 @@
+#include "fsm.h"
+#include <mega328p.h>
+
+void fsm(int *state,int input,int *output){
+    switch(*state){
+    case STATE_OFF1:{
+        if(input==1){
+            *state=STATE_ON0; 
+            *output=1;
+        }else{
+            // tetap 
+            *output=0;                   
+        }
+        break;
+    }
+    case STATE_ON0:{
+        if(input==1){
+            *output=1;
+        }else{
+            *state=STATE_ON1;
+            *output=1;        
+        }
+        break;
+    }
+    case STATE_ON1:{
+        if(input==1){
+            *state=STATE_OFF0;
+            *output=0;
+        }else{
+            // tetap
+            *output=1;        
+        }
+        break;
+    }
+    case STATE_OFF0:{
+        if(input==1){
+            *output=0;            
+        }else{
+            *state=STATE_OFF1;
+            *output=0;                    
+        }
+        break;
+    }
+    default:{
+        *state=STATE_OFF1;  // mestinya tidak pernah ke sini
+        break;
+        }        
+    }  
+
+}
---
+++ fsm-serial-toggle/fsm.h
@@ -0,0 +1,6 @@
+#define STATE_ON0   10
+#define STATE_ON1   11
+#define STATE_OFF0  12
+#define STATE_OFF1  13
+
+void fsm(int *state,int input,int *output);
